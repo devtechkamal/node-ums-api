7ed56c702fd1decadac8237e6fb304363ed2610c
devtechkamal <131899275+devtechkamal@users.noreply.github.com>
2025-03-09 13:00:51 +0530

    update crud operation
    update crud operation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import config from "./config";
 import routes from "./routes";
 import { errorHandler } from "./utils/errorHandler";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("", routes);
 
